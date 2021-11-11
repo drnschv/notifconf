503d57aba25a64265f7a5c56f15472f8829111e8
Adrián Schivo <adrian.schivo@softtek.com>
2021-11-11 21:09:49 +0000

    activacion masiva

--- webapp/controller/Worklist.controller.js
+++ webapp/controller/Worklist.controller.js
@@ -2,9 +2,8 @@ sap.ui.define([
 	"./BaseController",
 	"sap/ui/model/json/JSONModel",
 	"../model/formatter",
-	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
-], function (BaseController, JSONModel, formatter, Filter, FilterOperator) {
+    "sap/m/MessageBox"
+], function (BaseController, JSONModel, formatter, MessageBox) {
 	"use strict";
 
 	return BaseController.extend("profertil.notifconf.controller.Worklist", {
@@ -43,7 +42,6 @@ sap.ui.define([
         
 
         onPressAdd: function (oEvent) {
-            //sap.m.MessageToast.show("Agregar");
             this._showObject(null);
         },
 
@@ -107,6 +105,46 @@ sap.ui.define([
             
         },
 
+        onPressDesactivar: function (oEvent) {
+            //var oTable = this.byId("tableData");
+            //var iSelectedItems = oTable.getSelectedItems().length;
+            //sap.m.MessageToast.show("Desactivar " + iSelectedItems + " seleccionados"); 
+            var oItems = this.byId("tableData").getSelectedItems();
+            this.confirmarGrabar(oItems, 'N');
+
+        },
+
+        onPressActivar: function (oEvent) {
+            //var oTable = this.byId("tableData");
+            //var iSelectedItems = oTable.getSelectedItems().length;
+            //sap.m.MessageToast.show("Activar " + iSelectedItems + " seleccionados"); 
+            var oItems = this.byId("tableData").getSelectedItems();
+            this.confirmarGrabar(oItems, 'S');
+
+        },
+
+        onSelectionChange: function (oEvent) {
+            var oTable = this.byId("tableData");
+            var iSelectedItems = oTable.getSelectedItems().length;
+            if (iSelectedItems > 1) {
+                this.byId("btnDesactivarId").setEnabled(true);
+                this.byId("btnActivarId").setEnabled(true);
+            } else {
+                this.byId("btnDesactivarId").setEnabled(false);
+                this.byId("btnActivarId").setEnabled(false);
+            } 
+
+        },
+
+        onUpdateStarted: function (oEvent) {
+            console.log(oEvent.getParameters());
+            var oTable = this.byId("tableData");
+            oTable.getSelectedItems().forEach( item => {
+                oTable.setSelectedItem(item, false);
+            });
+
+        },
+
 		/**
 		 * Event handler for navigating back.
 		 * We navigate back in the browser history
@@ -114,7 +152,81 @@ sap.ui.define([
 		 */
 		onNavBack : function() {
 			history.go(-1);
-		},
+        },
+        
+
+        /* =========================================================== */
+		/* save methods                                                */
+		/* =========================================================== */
+        confirmarGrabar: function (aItems, sActiva) {
+            var iLength = aItems.length; 
+            var sMessage = "";
+            if (sActiva === 'S') {
+                var sMessage = this.getResourceBundle().getText("msgActivarTxt", iLength.toString());
+                var aActions = ["Continuar y Activarlas", "Cancelar"];
+            } else {
+                var sMessage = this.getResourceBundle().getText("msgDesactivarTxt", iLength.toString());
+                var aActions = ["Continuar y Desactivarlas", "Cancelar"];
+            }
+            
+            var that = this;
+            MessageBox.warning(sMessage, {
+                actions: aActions,
+                onClose: function (sAction) {
+                    if (sAction === "Cancelar") {
+                        sap.m.MessageToast.show("Accion Cancelada");
+                    } else {
+                        that.createEntries(aItems, sActiva);  
+                    }
+
+                }
+            });
+
+        },
+
+        createEntries: function (aDataObject, sActiva) { 
+            var aSuccess = [];
+            var aError = [];
+            var oModel = this.getView().getModel();
+            oModel.setUseBatch(false);
+            //var sPath = "/ConfiguracionSet";
+            var sActiva = bActiva ? "S" : "N";
+            var that = this;
+            var runCreate = function (dataIndex) {
+                if (aDataObject.length === dataIndex) {
+                    sap.m.MessageToast.show("Configuraciones actualizadas: " + aSuccess.length);
+                    //console.warn("OK: " + aSuccess.length + ", Errores: " + aError.length );
+                    that.onNavBack();
+                    that.getView().setBusy(false);
+                    return;
+                }
+
+                var sPath = "";
+                var oDataObject = {};
+                var oNewData = {};
+                oDataObject = aDataObject[dataIndex];
+                var sPath = oDataObject.getBindignContextPath();
+                oNewData.kunnr = oDataObject.getBindingContext().getObject().kunnr;
+                oNewData.rol = oDataObject.getBindingContext().getObject().rol;
+                oNewData.notifid = oDataObject.getBindingContext().getObject().notifid;
+                oNewData.activa = sActiva;
+
+                oModel.update(sPath, oNewData, {
+                    success: function (oData) {
+                        aSuccess.push(oNewData);
+                        runCreate(++dataIndex);
+                    },
+                    error: function (oError) {
+                        aError.push(oNewData);
+                        runCreate(++dataIndex);
+                    }
+                });                    
+
+            }
+            this.getView().setBusy(true);
+            runCreate(0);         
+
+        },
 
 		/* =========================================================== */
 		/* internal methods                                            */
@@ -127,90 +239,13 @@ sap.ui.define([
 		 * @private
 		 */
 		_showObject : function (oItem) {
-			//this.getRouter().navTo("object", {
-            //    kunnr: oItem.getBindingContext().getProperty("kunnr"),
-            //    rol: oItem.getBindingContext().getProperty("rol"),
-            //    notifid: oItem.getBindingContext().getProperty("notifid"),
-            //});
-
             this.getRouter().navTo("object");
             
         },
         
 
 
-        /* =========================================================== */
-		/* OLD event handlers                                          */
-		/* =========================================================== */
-
-        /**
-		 * Triggered by the table's 'updateFinished' event: after new table
-		 * data is available, this handler method updates the table counter.
-		 * This should only happen if the update was successful, which is
-		 * why this handler is attached to 'updateFinished' and not to the
-		 * table's list binding's 'dataReceived' method.
-		 * @param {sap.ui.base.Event} oEvent the update finished event
-		 * @public
-		 */
-		onUpdateFinished : function (oEvent) {
-			// update the worklist's object counter after the table update
-			var sTitle,
-				oTable = oEvent.getSource(),
-				iTotalItems = oEvent.getParameter("total");
-			// only update the counter if the length is final and
-			// the table is not empty
-			if (iTotalItems && oTable.getBinding("items").isLengthFinal()) {
-				sTitle = this.getResourceBundle().getText("worklistTableTitleCount", [iTotalItems]);
-			} else {
-				sTitle = this.getResourceBundle().getText("worklistTableTitle");
-			}
-			this.getModel("worklistView").setProperty("/worklistTableTitle", sTitle);
-		},
-
-
-        onSearch : function (oEvent) {
-			if (oEvent.getParameters().refreshButtonPressed) {
-				// Search field's 'refresh' button has been pressed.
-				// This is visible if you select any master list item.
-				// In this case no new search is triggered, we only
-				// refresh the list binding.
-				this.onRefresh();
-			} else {
-				var aTableSearchState = [];
-				var sQuery = oEvent.getParameter("query");
-
-				if (sQuery && sQuery.length > 0) {
-					aTableSearchState = [new Filter("kunnr", FilterOperator.Contains, sQuery)];
-				}
-				this._applySearch(aTableSearchState);
-			}
-
-		},
-
-        /**
-		 * Event handler for refresh event. Keeps filter, sort
-		 * and group settings and refreshes the list binding.
-		 * @public
-		 */
-		onRefresh : function () {
-			var oTable = this.byId("table");
-			oTable.getBinding("items").refresh();
-		},
-
-		/**
-		 * Internal helper method to apply both filter and search state together on the list binding
-		 * @param {sap.ui.model.Filter[]} aTableSearchState An array of filters for the search
-		 * @private
-		 */
-		_applySearch: function(aTableSearchState) {
-			var oTable = this.byId("table"),
-				oViewModel = this.getModel("worklistView");
-			oTable.getBinding("items").filter(aTableSearchState, "Application");
-			// changes the noDataText of the list in case there are no filter results
-			if (aTableSearchState.length !== 0) {
-				oViewModel.setProperty("/tableNoDataText", this.getResourceBundle().getText("worklistNoDataWithSearchText"));
-			}
-		}
+        
 
 	});
 });
